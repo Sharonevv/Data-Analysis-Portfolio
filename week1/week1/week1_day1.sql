26eecf969387d8c871918fb31cfdd93128e0d6e4
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-08-14 01:14:53 +0200

    week1_day1.sql

---
+++ week1/week1/week1_day1.sql
@@ -0,0 +1,24 @@
+-- Week 1 - Day 1 SQLBolt Practice
+-- By me: Sharone
+
+-- Lesson 1, Exercise 1
+-- Find the title of each film
+SELECT title FROM movies;
+
+-- Lesson 1, Exercise 2
+-- Find the director of each film
+SELECT director FROM movies;
+
+-- Lesson 1, Exercise 3
+-- Find the title and director of each film
+SELECT title, director FROM movies;
+
+-- Lesson 1, Exercise 4
+-- Find the title and year of each film
+SELECT title, year FROM movies;
+
+-- Lesson 1, Exercise 5
+-- Find all the information about each film
+SELECT * FROM movies;
+
+--Lesson 2, Excercise 1
