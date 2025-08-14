bb3157c7c5299d93623be28c4fe4a4fdc6fadbef
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-08-14 11:57:57 +0200

    week1_day1.sql

--- week1/week1/week1_day1.sql
+++ week1/week1/week1_day1.sql
@@ -21,9 +21,39 @@ SELECT title, year FROM movies;
 -- Find all the information about each film
 SELECT * FROM movies;
 
+
+
 --Lesson 2, Excercise 1
+--Find the movie with a row id of 6
+SELECT title FROM movies WHERE id = 6
+
+--Lesson 2, Exercise 2
+--Find the movies released in the years between 2000 and 2010 
+SELECT title FROM movies WHERE year BETWEEN 2000 AND 2010;
+
+--Lesson 2, Excercise 3
+--Find the movies not released in the years between 2000 and 2010
+SELECT title FROM movies WHERE year NOT BETWEEN 2000 AND 2010;
+
+--Lesson 2, Exercise 4
+--Find the first 5 Pixar movies and their release year
+SELECT title FROM movies WHERE year <= 2003;
+
+
 
 
 --Lesson 3, Exercise 1
 --Find all the Toy Story movies
- SELECT title FROM movies WHERE title = 'Toy Story'
+SELECT title FROM movies WHERE title LIKE "toy stor%";
+
+--Lesson 3, Exercise 2
+--Find all the movies directed by John Lasseter 
+SELECT title FROM movies WHERE Director ='John Lasseter';
+
+--Lesson 3, Exercise 3
+--Find all the movies (and director) not directed by John Lasseter
+SELECT title, director FROM movies WHERE director!='John Lasseter';
+
+--Lesson 3, Exercise 4
+--Find all the WALL-* movies
+SELECT title FROM movies WHERE title LIKE "WALL__";
