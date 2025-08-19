2c5a4c5a49f022d573ae370a97ae3cc016b469e5
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-08-19 13:05:02 +0200

    Update week1_day2.sql

--- week1/week1/week1_day2.sql
+++ week1/week1/week1_day2.sql
@@ -50,8 +50,21 @@ SELECT city, population FROM north_american_cities
 
 
 
--- Lesson 6 (      ) 
---Exercise 1, 
+
+--Lesson 6 Multi-table queries with JOINS (https://sqlbolt.com/lesson/select_queries_with_joins) 
+--Exercise 1, Find the domestic and international sales for each movie
+SELECT title, domestic_sales, international_sales
+  FROM movies JOIN boxoffice ON movies.id = boxoffice.movie_id;
+
+--Lesson 6, Exercise 2 
+--Show the sales numbers for each movie that did better internationally rather than domestically
+SELECT title, domestic_sales, international_sales FROM movies JOIN boxoffice ON movies.id = boxoffice.movie_id
+  WHERE international_sales > domestic_sales;
+
+--Lesson 6, Exercise 3 
+--List all the movies by their ratings in descending order
+SELECT title, rating FROM movies JOIN boxoffice ON movies.id = boxoffice.movie_id 
+  ORDER BY rating DESC;
 
 
 
