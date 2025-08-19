78b6eb847841184e23fe009e79478bff9e842fa1
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-08-19 12:40:52 +0200

    Update week1_day2.sql

--- week1/week1/week1_day2.sql
+++ week1/week1/week1_day2.sql
@@ -1,1 +1,19 @@
+--Lesson 4 Exercise 1 (https://sqlbolt.com/lesson/filtering_sorting_query_results
+--List all directors of Pixar movies (alphabetically), without duplicates
+SELECT DISTINCT director FROM movies
+  ORDER BY director;
 
+--Lesson 4, Exercise 2
+--List the last four Pixar movies released (ordered from most recent to least)
+SELECT title, year FROM movies
+  ORDER BY year DESC LIMIT 4;
+
+--Lesson 4, Exercise 3
+--List the first five Pixar movies sorted alphabetically
+SELECT title FROM movies 
+  ORDER BY title ASC LIMIT 5;
+
+--Lesson 4, Exercise 4
+--List the next five Pixar movies sorted alphabetically
+SELECT title FROM movies
+  ORDER BY title ASC LIMIT 5 OFFSET 5;
