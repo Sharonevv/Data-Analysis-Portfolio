847da3cdde4159ac0ffe96a09c230aed485ff55b
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-08-20 10:08:45 +0200

    Update week1_day3.sql

--- week1/week1/week1_day3.sql
+++ week1/week1/week1_day3.sql
@@ -1,13 +1,20 @@
 -- Week 1 - Day 3 SQLBolt Practice
 
 -- Lesson 7 OUTTER JOINs (https://sqlbolt.com/lesson/select_queries_with_outer_joins)
--- Exercise 1,
+-- Exercise 1, Find the list of all buildings that have employees 
+SELECT DISTINCT building FROM employees;
 
+--Lesson 7, Exercise 2
+--Find the list of all buildings and their capacity
+SELECT * FROM buildings 
 
-
-
-
+--Lesson 7, Exercise 3
+--List all buildings and the distinct employee roles in each building (including empty buildings)
 SELECT DISTINCT building_name, role 
 FROM buildings 
   LEFT JOIN employees
     ON building_name = building;
+
+
+--Lesson 8 (      ) 
+--Exercise 1, hi
