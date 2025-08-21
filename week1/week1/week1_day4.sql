ab1896fdb63e1b0f3247646541551ff4647c1788
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-08-21 12:18:37 +0200

    Update week1_day4.sql

--- week1/week1/week1_day4.sql
+++ week1/week1/week1_day4.sql
@@ -4,3 +4,12 @@
 --Exercise 1 Find the longest time that an employee has been at the studio 
 SELECT MAX (Years_employed) AS studio_time
   FROM employees; 
+
+--Lesson 10, Exercise 2
+--For each role, find the average number of years employed by employees in that role
+SELECT role, AVG(years_employed) as Avg_yrs_employed
+FROM employees
+GROUP BY role;
+
+--Lesson 10, Exercise 3
+--Find the total number of employee years worked in each building
