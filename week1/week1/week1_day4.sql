b16ff5a9e63e5637a5414e5d402027fb62149723
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-08-21 12:07:08 +0200

    Update week1_day4.sql

--- week1/week1/week1_day4.sql
+++ week1/week1/week1_day4.sql
@@ -1,6 +1,6 @@
 
 
---Lesson 10 hi (     ) 
---Exercise 1 hi
+--Lesson 10 Queries with aggregates (Pt. 1) (https://sqlbolt.com/lesson/select_queries_with_aggregates) 
+--Exercise 1 Find the longest time that an employee has been at the studio 
 SELECT MAX (Years_employed) AS studio_time
   FROM employees; 
