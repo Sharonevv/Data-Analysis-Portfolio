4f98514b4504278a9f1a7760e7e852b204fd2b80
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-08-28 12:30:57 +0200

    Update week1_day5.sql

--- week1/week1/week1_day5.sql
+++ week1/week1/week1_day5.sql
@@ -7,3 +7,20 @@ SELECT director, COUNT(title) AS movies_per_director
 
 --Lesson 12, Exercise 2
 --Find the total domestic and international sales that can be attributed to each director
+SELECT director, SUM(domestic_sales + international_sales) AS  gross_sales_per_director FROM movies
+  JOIN Boxoffice ON movies.Id = Boxoffice.movie_Id GROUP BY director;
+
+
+
+
+
+--Lesson 13 Inserting rows (https://sqlbolt.com/lesson/inserting_rows)
+--Exercise 1, Add the studio's new production, Toy Story 4 to the list of movies (you can use any director)
+
+
+--Lesson 13, Exercise 2
+--Toy Story 4 has been released to critical acclaim! It had a rating of 8.7, and made 340 million domestically and 270 million internationally. 
+--Add the record to the BoxOffice table.
+
+
+
