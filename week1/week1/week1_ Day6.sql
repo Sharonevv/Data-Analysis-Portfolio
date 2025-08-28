a2ef0208dc0b7c2ba0f10288fe390d689720dbbe
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-08-28 14:25:46 +0200

    Update week1_ Day6.sql

--- week1/week1/week1_ Day6.sql
+++ week1/week1/week1_ Day6.sql
@@ -16,7 +16,8 @@ CREATE TABLE Database (
 
 --Lesson 17 Altering tables (https://sqlbolt.com/lesson/altering_tables)
 --Exercise 1, Add a column named Aspect_ratio with a FLOAT data type to store the aspect-ratio each movie was released in.
---Add another column named Language with a TEXT data type to store the language that the movie was released in.
+ALTER TABLE movies ADD COLUMN Aspect_Ratio FLOAT;
 
 
---Lesson 17, Exercise 2Ensure that the default for this language is English
+--Lesson 17, Exercise 2 Add another column named Language with a TEXT data type to store the language that the movie was released in.
+--Ensure that the default for this language is English
