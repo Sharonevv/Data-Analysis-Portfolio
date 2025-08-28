dcebbc54c0bae91200f215ea2b05860ce05aaf34
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-08-28 12:21:46 +0200

    Update week1_day5.sql

--- week1/week1/week1_day5.sql
+++ week1/week1/week1_day5.sql
@@ -1,1 +1,9 @@
 
+
+--Lesson 12 Order of execution of a Query  (https://sqlbolt.com/lesson/select_queries_order_of_execution) 
+--Exercise 1, Find the number of movies each director has directed
+SELECT director, COUNT(title) AS movies_per_director
+  FROM movies GROUP BY director; 
+
+--Lesson 12, Exercise 2
+--Find the total domestic and international sales that can be attributed to each director
