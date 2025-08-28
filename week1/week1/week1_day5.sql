f2125b2875d64055b2ab41ee51b0cbf3e9dd7aed
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-08-28 12:47:05 +0200

    Update week1_day5.sql

--- week1/week1/week1_day5.sql
+++ week1/week1/week1_day5.sql
@@ -16,6 +16,7 @@ SELECT director, SUM(domestic_sales + international_sales) AS  gross_sales_per_d
 
 --Lesson 13 Inserting rows (https://sqlbolt.com/lesson/inserting_rows)
 --Exercise 1, Add the studio's new production, Toy Story 4 to the list of movies (you can use any director)
+INSERT INTO movies VALUES(4, "Toy Story 4","Sharone Boois", 2025, 135);
 
 
 --Lesson 13, Exercise 2
