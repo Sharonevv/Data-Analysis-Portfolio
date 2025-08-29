60701d8deffd9459fcc8a2497c7923face704da0
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-08-29 23:35:54 +0200

    Update week2_restaurant_orders.sql

--- week2_restaurant_orders.sql
+++ week2_restaurant_orders.sql
@@ -43,3 +43,57 @@ SELECT * FROM restaurant_orders WHERE CAST (Order Time AS Time) >'12:00';
 SELECT * FROM restaurant_orders WHERE Price BETWEEN 17 AND 23;
 
 
+--TOPIC 3: SORTING Results
+
+--11. Write a query to select all orders and sort them by price in ascending order.
+SELECT * FROM restaurant_orders ORDER BY price ASC;
+
+--12. Write a query to select all orders and sort first by quantity descending, then by price ascending.
+SELECT * FROM restaurant_orders ORDER BY Quantity DESC, Price ASC;
+
+--13. Write a query to select all orders and sort them alphabetically by customer name.
+SELECT * FROM restaurant_orders ORDER BY Customer Name ASC;
+
+
+--TOPIC 4: AGGREGATION Functions 
+
+--14. Write a query to count the total number of orders in restaurant_orders.
+SELECT COUNT(*) AS Total_num_orders FROM restaurant_orders; 
+
+--15. Write a query to find the maximum price among all orders.
+SELECT MAX(Price) AS Max_order_price FROM restaurant_orders;
+
+--16. Write a query to find the minimum quantity among all orders.
+SELECT MIN(Quantity) AS Min_order_quantity FROM restaurant_orders; 
+
+--17. Write a query to calculate the average price of all orders.
+SELECT AVG(Price) AS Avg_order_price FROM restaurant_orders;
+
+--18. Write a query to calculate the total quantity of items ordered.
+SELECT SUM(Quantity) AS Sum_of_quantity FROM restaurant_orders;
+
+
+--TOPIC 4: GROUP BY
+
+--19. Write a query to calculate the total quantity ordered for each category.
+SELECT Category, SUM(Quantity) AS total_quantity 
+  FROM restaurant_orders GROUP BY Category; 
+
+--20. Write a query to calculate the average price for each food item.
+SELECT Food Item, AVG(Price) AS Avg_prics 
+  FROM restaurant_orders GROUP BY Food Item;
+
+--21. Write a query to count the number of orders for each payment method.
+SELECT COUNT(*) AS Num_orders FROM restaurant_orders
+  GROUP BY Payment Method;
+
+--22. Write a query to find the highest price in each category.
+SELECT Category, MAX(Price) AS Max_price FROM restaurant_orders 
+  GROUP BY Category;
+
+
+
+
+
+
+
