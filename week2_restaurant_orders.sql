34fbd0a422e308593d8fa2434ba00e52f4a96de0
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-08-29 20:42:41 +0200

    Update and rename week2 to week2_restaurant_orders.sql

---
+++ week2_restaurant_orders.sql
@@ -0,0 +1,12 @@
+/* 
+This is the the SQL code for the project 
+restaurant_orders table I did. I got the dataset from kiggle 
+to help tackle real world data 
+*/
+
+--1. Write a query to select all columns  and all rows from the restaurant_orders table.
+SELECT * FROM restaurant_orders;
+
+--2. Write a query to select only the customer name and food item columns from restaurant_orders table.
+SELECT Customer Name, Food Item FROM restaurant_orders;
+
