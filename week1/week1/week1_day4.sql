fe65a3d3e0ffaa773af66d8c4ab471f0e894d425
Sharonevv <74068921+Sharonevv@users.noreply.github.com>
2025-12-20 12:10:18 +0200

    Add analysis of employee data using SQL aggregates
    Analyzed employee data using SQL aggregate functions to summarize workforce metrics and identify patterns across roles and buildings. Applied MAX, AVG, SUM, and COUNT alongside GROUP BY and HAVING clauses to evaluate employee tenure, role distribution, and organizational capacity.

--- week1/week1/week1_day4.sql
+++ week1/week1/week1_day4.sql
@@ -1,4 +1,7 @@
 -- Week 1 - Day 4 SQLBolt Practice
+I Analyzed employee data using SQL aggregate functions to summarise workforce metrics and identify patterns across roles 
+and buildings. Applied MAX, AVG, SUM, and COUNT alongside GROUP BY and HAVING clauses 
+to evaluate employee tenure, role distribution, and organisational capacity.
 
 --Lesson 10 Queries with aggregates (Pt. 1) (https://sqlbolt.com/lesson/select_queries_with_aggregates) 
 --Exercise 1 Find the longest time that an employee has been at the studio 
